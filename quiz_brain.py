27b501d97c1e8de546fdaab8124c6d22a61e9f7e
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-11-10 14:35:27 -0800

    check_answer method

--- quiz_brain.py
+++ quiz_brain.py
@@ -8,7 +8,19 @@ class QuizBrain:
     def next_question(self):
         current_question = self.question_list[self.question_number]
         self.question_number += 1
-        input(f"Q.{self.question_number}: {current_question.text} (True/False)?: ")
+        answer = input(
+            f"Q.{self.question_number}: {current_question.text} (True/False)?: ")
+        self.check_answer(answer, current_question.answer)
 
     def still_has_questions(self):
         return self.question_number < len(self.question_list)
+
+    def check_answer(self, user_answer, correct_answer):
+        if correct_answer.lower() == user_answer.lower():
+            self.score += 1
+            print("Correct!")
+        else:
+            print("Incorrect.")
+        print(f"The correct answer is: {correct_answer}.")
+        print(f"Your current score is: {self.score}/{self.question_number}")
+        print("\n")
