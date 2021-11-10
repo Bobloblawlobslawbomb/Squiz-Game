2066c0b4f4c0dc2a0903ab05c53f39b279c71443
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-11-10 14:28:01 -0800

    quiz var - which is a QuizBrain obj

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from question_model import Question
+from quiz_brain import QuizBrain
 from data import question_data
 
 question_bank = []
@@ -8,3 +9,5 @@ for question in question_data:
     question_answer = question["answer"]
     new_question = Question(question_text, question_answer)
     question_bank.append(new_question)
+
+quiz = QuizBrain(question_bank)
